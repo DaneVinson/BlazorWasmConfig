3a8063f388a7656b5b2945df035f23845e91251a
Dane Vinson <dane.vinson@sprbrk.com>
2020-02-28 23:03:25 -0800

    Initial

---
+++ BlazorWasmConfig/Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using BlazorWasmConfig.Shared;
+using System.Reflection;
+using System.Linq;
+using System.Net.Http;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorWasmConfig.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                                        .AddJsonStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("BlazorWasmConfig.Client.appsettings.json"))
+                                        .Build();
+
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddTransient(_ => { return configuration.GetSection("GalaxyStuff").Get<GalaxyInfo>(); });
+
+            builder.RootComponents.Add<App>("app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ BlazorWasmConfig/Shared/GalaxyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorWasmConfig.Shared
+{
+    public class GalaxyInfo
+    {
+        public string GalaxyCluster { get; set; }
+        public string GalaxyName { get; set; }
+        public long StarCount { get; set; }
+
+        public override string ToString()
+        {
+            return $"Galaxy: {GalaxyName}, Cluster: {GalaxyCluster}, Stars: {StarCount}";
+        }
+    }
+}
